cfdf6e57d8b26b47fa3638e91db46f4a435280fe
paolotiu17 <paolotiu17@gmail.com>
2020-10-02 23:17:07 +0800

    Updates

---
+++ precache-manifest.13963c85fb3c7149be9367fd2ccb723d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa64792bb446be783120267d026413fb",
+    "url": "/memory-card/index.html"
+  },
+  {
+    "revision": "da988426e144530d6baa",
+    "url": "/memory-card/static/css/main.29c6eda6.chunk.css"
+  },
+  {
+    "revision": "98d9d292fe632d2adc23",
+    "url": "/memory-card/static/js/2.6ee6c3f0.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/memory-card/static/js/2.6ee6c3f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da988426e144530d6baa",
+    "url": "/memory-card/static/js/main.da8e3723.chunk.js"
+  },
+  {
+    "revision": "ce1e1b1de73b9dc64c4e",
+    "url": "/memory-card/static/js/runtime-main.fac9530b.js"
+  }
+]);
